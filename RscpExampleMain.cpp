ccd62f9dae0f2eef21bd811d75f1e0a224e57e34
Eberhard Mayer <eberhardmayer@eberhards-air-5.fritz.box>
2020-03-15 11:51:00 +0100

    kleine Anpassung

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -346,7 +346,8 @@ int LoadDataProcess(SRscpFrameBuffer * frameBuffer) {
         
 //        if (((abs( int(iPower - iPower_Bat)) > 30)||(t%3600==0))&&(iLMStatus == 1))
 //            if (((abs( int(iPower - iBattLoad)) > 30)||(abs(t-tE3DC_alt)>3600*3))&&(iLMStatus == 1))
-          {
+    if (iLMStatus == 1)
+    {
         
             {
             if (iBattLoad > (iPower_Bat-iDiffLadeleistung))
