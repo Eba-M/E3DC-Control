bab3edf2fc15bd9ca381fbfbd166326434383289
Eba_M <eberhard.mayer@gmx.net>
2023-09-13 15:51:44 +0200

    commit

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -3280,6 +3280,7 @@ static void mainLoop(void)
             aWATTar(ch,e3dc_config.AWLand,e3dc_config.AWMWSt,e3dc_config.AWNebenkosten); // im Master nicht aufrufen
 //            test;
 //
+            
 //            if (e3dc_config.WP)
 //              mewp(fatemp);
             if((frameBuffer.dataLength == 0)&&(e3dc_config.wallbox>=0)&&(bWBRequest))
