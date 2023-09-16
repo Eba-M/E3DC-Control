6a606b7a0a54c4f176b33200488f650a2adfc86e
Eba_M <eberhard.mayer@gmx.net>
2023-09-16 10:26:32 +0200

    update

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -1205,6 +1205,7 @@ bDischarge = false;
     // Temperaturen der oekofen ausgeben
     if (strcmp(e3dc_config.heizung_ip,"")>0)
     {
+        printf("oekofen AT %i HK1 %i %i %i %i %i %i ",temp[0],temp[1],temp[2],temp[3],temp[4],temp[5],temp[6]);
         printf("HK2 %i %i %i %i %i %i"
                ,temp[7],temp[8],temp[9],temp[10],temp[11],temp[12]);
         printf("%c[K\n", 27 );
