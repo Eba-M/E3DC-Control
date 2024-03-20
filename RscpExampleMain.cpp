41c15357a86a805e94183bd9cfb50ebc3d8d451a
Eba_M <eberhard.mayer@gmx.net>
2024-03-20 16:19:33 +0100

    Wolf

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -1676,15 +1676,16 @@ int LoadDataProcess() {
                     wp_t = t;
                 }   else
                 
-                if (
+                if 
                     (
-//                    PVon < 0 &&
-                    (
-                     (temp[1]>0&&temp[4]<temp[5])
+                     (
+                    PVon < 0 &&
+                      (
+                       (temp[1]>0&&temp[4]<temp[5])
                      ||
-                     (temp[7]>0&&temp[10]<temp[11])
-                    )
+                       (temp[7]>0&&temp[10]<temp[11])
                       )
+                     )
                     ||
                     temp[14]>450
                     ||
