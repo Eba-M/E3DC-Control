10d7d3bb231d2cf9343e77cab6b473298d88a617
Eba_M <eberhard.mayer@gmx.net>
2024-04-17 16:57:52 +0200

    Wolf

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -1744,7 +1744,7 @@ int LoadDataProcess() {
                 // muss Leistung angehoben werden?
                 int mm=t%(24*3600)/60;
                 if (
-                    (temp[1]>0&&temp[6]>0&&temp[4]>temp[5]+10)
+                    (temp[1]>0&&temp[6]>0&&temp[4]>(temp[5]+10)&&temp[14]<(e3dc_config.WPHK1max+5))
                     ||
                     (temp[7]>0&&temp[10]>temp[11]+10)
                     ||
