9e0935b2a0af4bdbb3a66d9667d6677bafe1d0e4
Eba_M <eberhard.mayer@gmx.net>
2024-05-25 11:15:36 +0200

    statistik

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -4045,7 +4045,7 @@ int handleResponseValue(RscpProtocol *protocol, SRscpValue *response)
                     int x3 = (t_alt%(24*7*4*900))/900;
                     if (x1 == 0) x1 = dayhour-1; else x1--;
                     if (x3 == dayhour) x3 = 0; else x3++;
-                    printf(" %0.04f/%0.04f/%0.04f %0.04fkWh",iWeekhour[x1]/3600000.0,iWeekhour[x2]/3600000.0,iWeekhour[x3]/3600000.0,iWeekhour[dayhour]/3600000.0); // Tages Hausverbrauch
+                    printf(" %0.04f/%0.04f/%0.04f %0.04f  %0.04fkWh",iWeekhour[x1]/3600000.0,iWeekhour[x2]/3600000.0,iWeekhour[x3]/3600000.0,iWeekhour[dayhour-1]/3600000.0,iWeekhour[dayhour]/3600000.0); // Tages Hausverbrauch
                 }
                 printf("%c[K\n", 27 );
 
