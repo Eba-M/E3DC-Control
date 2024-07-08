9e15d28ae5f156089d628b8a1d49ab19dd0b43f2
Eba_M <eberhard.mayer@gmx.net>
2024-07-08 22:46:00 +0200

    point

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -3076,7 +3076,7 @@ bDischarge = false;
                     
                     {
                         // peakshaving erforderlich?
-                        if (e3dc_config.peakshave > 0)
+/*                        if (e3dc_config.peakshave > 0)
                         {
                             // Im Steuerbereich = Zielbereich - 500 in diesem Bereich wird gesteuert
                             if ((fPower_Grid) > e3dc_config.peakshave-500)
@@ -3108,6 +3108,7 @@ bDischarge = false;
                                 
                             }
                         };
+*/
                         if (iLMStatus == 1){
                             if
                                 (
