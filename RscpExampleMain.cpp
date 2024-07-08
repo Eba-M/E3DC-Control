adfb39aeac108de7619232a375d44b0419c90f80
Eba_M <eberhard.mayer@gmx.net>
2024-07-08 19:29:10 +0200

    point

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -2893,7 +2893,7 @@ bDischarge = false;
         {
             iFc = fBatt_SOC*e3dc_config.speichergroesse*10*3600;
             iFc = iFc / idauer *-1;
-            iFc = iFc + iPower_PV_E3DC + fPower_Ext[1] + fPower_Ext[2];
+            iFc = iFc + iPower_PV_E3DC - fPower_Ext[1] - fPower_Ext[2];
             if (iFc > 0)
                 iFc = e3dc_config.maximumLadeleistung; // noch kein shaving
             else
