1276e368e2a5e06d1d0a8be5272311484c5f1c5c
Eba_M <eberhard.mayer@gmx.net>
2024-07-08 19:42:46 +0200

    point

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -2894,8 +2894,11 @@ bDischarge = false;
             iFc = fBatt_SOC*e3dc_config.speichergroesse*10*3600;
             iFc = iFc / idauer *-1;
             iFc = iFc + iPower_PV_E3DC - fPower_Ext[1] - fPower_Ext[2];
-            if (iFc > 0)
+            if (iFc > 0||iPower_PV>iPowerHome)
+            {
                 iFc = e3dc_config.maximumLadeleistung; // noch kein shaving
+                average = 0;
+            }
             else
             {
                 average = average * .99 + iFc/100;
