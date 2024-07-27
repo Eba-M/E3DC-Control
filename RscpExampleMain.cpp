bd25f70d70ab560c6b8a4e9750f6f7f04b687b75
Eba_M <eberhard.mayer@gmx.net>
2024-07-27 07:56:11 +0200

    peakshave

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -3013,7 +3013,7 @@ bDischarge = false;
         {
 // Tagesbetrieb
             fpeakshaveminsoc = (sunsetAt-sunriseAt)*60+2*e3dc_config.unload*60; //regeldauer
-            fpeakshaveminsoc = (t-itime2-0*3600)/(fpeakshaveminsoc-0*3600);      //% restregeldauer
+            fpeakshaveminsoc = (t-itime2-1*3600)/(fpeakshaveminsoc-1*3600);      //% restregeldauer
             // Beginn um 3h nach hinten verschieben
             fpeakshaveminsoc = (e3dc_config.peakshaveuppersoc-e3dc_config.peakshavesoc)*fpeakshaveminsoc+e3dc_config.peakshavesoc;
         } else // Nachtbetrieb
@@ -3040,6 +3040,8 @@ bDischarge = false;
         static int MQTTAval;
         if (
             (idauer > 0
+             ||
+             fBatt_SOC<e3dc_config.peakshavesoc
              ||
              fBatt_SOC<fpeakshaveminsoc
              ||
