64d012e39cea014d45b901d795e82268d2daa3bc
Eba_M <eberhard.mayer@gmx.net>
2024-08-03 09:31:45 +0200

    iPower_Bat

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -1499,7 +1499,7 @@ if ((e3dc_config.MQTTavl > 0)&&(tE3DC % e3dc_config.MQTTavl) == 0)
     MQTTsend(e3dc_config.mqtt2_ip,buf);
     sprintf(buf,"E3DC-Control/BattL -m '%i' ",iBattLoad);
     MQTTsend(e3dc_config.mqtt2_ip,buf);
-    sprintf(buf,"E3DC-Control/Grid -m '%0.2f %0.2f %0.2f'",fPower_Grid,fBatt_SOC,iPower_Bat);
+    sprintf(buf,"E3DC-Control/Grid -m '%0.2f %0.2f %0.2f'",fPower_Grid,fBatt_SOC,float(iPower_Bat));
     MQTTsend(e3dc_config.mqtt2_ip,buf);
 
     if (e3dc_config.debug) printf("D4b");
