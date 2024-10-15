cdfedca1771ca7a14585522a71d0c99f73e9f905
Eba_M <eberhard.mayer@gmx.net>
2024-10-15 17:30:39 +0200

    WP HK1

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -1168,7 +1168,7 @@ int iModbusTCP()
                     )
 // EVU aus und Kessel aus ODER fbh Anforderung aus aber  Heizkreis aktiv -> HK ausschalten
                 {
-                    iLength  = iModbusTCP_Set(11,0,11); //FBH?
+//                    iLength  = iModbusTCP_Set(11,0,11); //FBH?
                     iLength  = iModbusTCP_Get(11,1,11); //FBH?
 //                    brequest = true;
                 }
