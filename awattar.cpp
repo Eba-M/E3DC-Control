46bdf6c42a2206240ae54f42c8ae491cb158b568
Eba_M <eberhard.mayer@gmx.net>
2024-10-28 14:45:30 +0100

    prog PV

--- awattar.cpp
+++ awattar.cpp
@@ -845,11 +845,21 @@ void openmeteo(std::vector<watt_s> &w,std::vector<wetter_s>  &wetter, e3dc_confi
                             wetter[x2].solar = wetter[x2].progsolar*f6;
 
                     }
-
                     if (wetter[x2].solar<f8&&wetter[x2].progsolar*f7>f8
-                        &&(wetter[x2].hh-wetter[0].hh)<12*3600)    // 12h
+                        &&
+                        (wetter[x2].hh-wetter[0].hh)<12*3600)    // 12h
                             (wetter[x2].solar=(2*f8+wetter[x2].progsolar*f7)/3);
                     else
+                        if (wetter[x2].solar<f8&&wetter[x2].progsolar>f8
+                            &&
+                            (wetter[x2].hh-wetter[0].hh)<12*3600)    // 12h
+                                (wetter[x2].solar=(2*f8+wetter[x2].progsolar)/3);
+                        else
+                            if (wetter[x2].solar<f8&&wetter[x2].progsolar<f8
+                                &&
+                                (wetter[x2].hh-wetter[0].hh)<12*3600)    // 12h
+                                    (wetter[x2].solar=(f8+wetter[x2].progsolar)/2);
+                            else
                         wetter[x2].solar = wetter[x2].progsolar*f7;
                     x1++;
                     x2++;
