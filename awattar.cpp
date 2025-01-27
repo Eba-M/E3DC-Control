e8473af0a412293ae5cd9c1bffa00afe270d16d5
Eba_M <eberhard.mayer@gmx.net>
2025-01-27 12:53:11 +0100

    epexspot

--- awattar.cpp
+++ awattar.cpp
@@ -1378,7 +1378,7 @@ if (e3dc.AWLand == 2)
     }
 
         // Abfragen EPEXSPOT
-                if (w.size()<4*12&&ptm->tm_hour*60+ptm->tm_min>12*60+50&&ptm->tm_hour<=22)
+                if (w.size()<=4*12&&ptm->tm_hour*60+ptm->tm_min>12*60+50&&ptm->tm_hour<=22)
                 {
 // Inhalt prophylaktisch löschen
                     fp = fopen("epexspot.txt","w");
