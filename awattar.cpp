86ae1533d5734aba6f493ad676eaad318f6c560a
Eba_M <eberhard.mayer@gmx.net>
2025-06-16 09:54:08 +0200

    Wallbox mode 1

--- awattar.cpp
+++ awattar.cpp
@@ -848,7 +848,8 @@ void openmeteo(std::vector<watt_s> &w,std::vector<wetter_s>  &wetter, e3dc_confi
                     float f4 = (iDayStat[199]) * e3dc.speichergroesse/10000.0;
                     float f5 = iDayStat[198]/3600.0/1000.0;
                     float f6 = 1;
-                    if (f4>1&&f5>0&&(wetter[x2].hh-wetter[0].hh)<12*3600) // erst nach der ersten kWh
+//                    if (f4>1&&f5>0&&(wetter[x2].hh-wetter[0].hh)<12*3600) // erst nach der ersten kWh
+                        if (f4>1&&f5>0) // erst nach der ersten kWh
                         f6 = f5/f4;
                     if (f6<0.1) f6 = 0.1;  // schneebedeckte Module?
                     if (f6>10) f6 = 10;
