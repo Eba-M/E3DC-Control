12e5da9dd2fb61ec4afb2d8651d5523149db2078
Eba_M <eberhard.mayer@gmx.net>
2025-08-14 15:39:59 +0200

    wbmode 1

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -4827,7 +4827,7 @@ int WBProcess(SRscpFrameBuffer * frameBuffer) {
                 //                iPower = -fPower_Grid-e3dc_config.einspeiselimit*1000;
                 if (fPower_WB > 100)
                 {
-                iPower = -fPower_Grid-e3dc_config.einspeiselimit*1000+500+iWBMinimumPower/6+fPower_WB;
+                iPower = -fPower_Grid-e3dc_config.einspeiselimit*1000+500+iPower_Bat+iWBMinimumPower/6+fPower_WB;
 //                    iPower = -fPower_Grid-e3dc_config.einspeiselimit*1000+500-iRefload+iWBMinimumPower/6-fPower_WB;
                 float fDiff = iPower_PV_E3DC - e3dc_config.maximumLadeleistung;
                     if (fDiff > 0)
