78c6f01a409e59ef4b0a0b66a90bd254d3a2c78f
Eba_M <eberhard.mayer@gmx.net>
2025-10-16 10:21:43 +0200

    Wolf

--- awattar.cpp
+++ awattar.cpp
@@ -875,7 +875,7 @@ void openmeteo(std::vector<watt_s> &w,std::vector<wetter_s>  &wetter, e3dc_confi
 // absoluter Ertrag des letzen 15min
                     float f8 = iDayStat[197] /(e3dc.speichergroesse*10*3600);
                     f8 = f8 * (10 - x2)/10;
-                    
+                    fprintf(fp1,"f6 %0.2f f7 %0.2f %i ",f6,f7,x2);
                     // relativer ertrag aus statistik höher als aktueller ertrag
                     if (f4 > 1&&x2<10)
                         f6 = (f7*(x2+1)+(10-x2)*f6)/(11);
@@ -884,6 +884,8 @@ void openmeteo(std::vector<watt_s> &w,std::vector<wetter_s>  &wetter, e3dc_confi
                             f6=f7;
 //                    if (x2<10)
 //                    f6 = (f6*(x2)+f8*(10-x2))/10;
+
+                    fprintf(fp1,"f6 %0.2f f7 %0.2f ",f6,f7);
 
                     if (anlage==0){
                         wetter[x2].progsolar = item2->valuedouble*x3/4/e3dc.speichergroesse/10;
