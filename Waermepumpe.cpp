a6a27d6f29ce978de1a11debe1e1517d6db488a9
Eba_M <eberhard.mayer@gmx.net>
2026-05-13 22:49:07 +0200

    DV

--- Waermepumpe.cpp
+++ Waermepumpe.cpp
@@ -638,6 +638,8 @@ void mewp(std::vector<watt_s> &w,std::vector<watt_s> &e,std::vector<wetter_s>&we
 //                                    if (wetter[x1].solar==0)
 //                                        fsoc = fsoc - wetter[x1].hourly;
                                     float highpp;
+                                    if (e3dc.debug) printf("NW6a\n");
+
                                     if (x1>=e.size()||wetter[x1].hourly+e3dc.WPmin/e3dc.speichergroesse*25>wetter[x1].solar||not e3dc.DV)
                                         highpp = w[x1].pp;
                                     else
