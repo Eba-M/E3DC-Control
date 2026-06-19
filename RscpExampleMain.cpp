556e510dff79a72feb674dbf16ab6459ada48740
Eba_M <eberhard.mayer@gmx.net>
2026-06-19 18:38:56 +0200

    ras

--- RscpExampleMain.cpp
+++ RscpExampleMain.cpp
@@ -4973,9 +4973,9 @@ bDischarge = false;
                     printf("#  3 %5.2f %2i\n",fpeakshaveminsoc,iFc);
 
                 printf("%c[K\n", 27 );
-                printf("shavingB = %i %.2f %i %2.0f %2.0f",t-itime,fpeakshaveminsoc,iFc,fPower_Ext[2],fPower_Ext[3]);
+                printf("shavingB = %li %5.2f %5i %2.0f %2.0f\n",t-itime,fpeakshaveminsoc,iFc,fPower_Ext[2],fPower_Ext[3]);
                 if (e3dc_config.debug1)
-                    printf("#  4 %5.2f %2i\n",fpeakshaveminsoc,iFc);
+                    printf("#  4 %5.2f %5i\n",fpeakshaveminsoc,iFc);
 
             }
             iFc0 = iFc;
